ae105066b5bad62aaf8aff24d1da8069d67360cd
Kang <ksung91889@naver.com>
2023-10-29 13:30:17 +0900

    레알 zoom 시작

--- src/public/js/appzoom.js
+++ src/public/js/appzoom.js
@@ -1,7 +1,7 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
-// 유저러보투 stream을 받아야함.
+// 유저로부터 stream을 받아야함.
 // stream은 비디오와 오디오가 결합된거.
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
@@ -10,9 +10,9 @@ let myStream;
 
 async function getMedia() {
   try {
-    // 유저의 유저미디어 string을 줌
+    // 아래 코드는 유저의 유저미디어 string을 줌
     myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
+      audio: true, // constraints. 우리가 얻고싶어하는 것들(여기선 오디오,비디오)
       video: true
     });
     console.log(myStream);
